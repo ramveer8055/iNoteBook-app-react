79d0f7911f29b358ccd3ae4c4eb6c715b1376b03
ramveer998 <ramveerch@student.iul.ac.in>
2022-10-29 19:13:20 +0530

    add note & fetch notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,52 @@
 const express = require('express')
 const router = express.Router();
+const fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-    obj = []
-    res.json(obj)
+//Route1: Get all the notes using Get "api/auth/fetchallnotes". login required
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const note = await Note.find({ user: req.user.id })
+        res.json(note)
+    } catch (error) {
+        console.log(error.messaage)
+        res.status(500).send("Internal server error");
+    }
+})
+
+//Route2: Add a new notes using Post "api/auth/addnotes". login required
+router.post('/addnotes', fetchuser, [
+    body('title', 'enter a title atleast 5 character').isLength({ min: 5 }),
+    body('description', 'enter a description atleast 10 character').isLength({ min: 10 }),
+], async (req, res) => {
+
+    try {
+
+
+        // if there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { title, description, tag } = req.body;
+
+
+        notes = new Note({
+            user: req.user.id,
+            title,
+            description,
+            tag
+        })
+
+        const saveNote = await notes.save();
+
+        res.json(saveNote)
+    } catch (error) {
+        console.log(error.messaage)
+        res.status(500).send("Internal server error");
+    }
 })
 
 module.exports = router
